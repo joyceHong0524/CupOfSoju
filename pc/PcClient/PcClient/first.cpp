202d8e6e951e9863c890fda37029a135e5235cf4
dddgg9511 <39615281+dddgg9511@users.noreply.github.com>
2019-06-27 02:34:28 +0900

    firstbuild

---
+++ pc/PcClient/PcClient/first.cpp
@@ -0,0 +1,21 @@
+#include "first.h"
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+
+
+[STAThreadAttribute]
+
+void Main(array<String^>^ args) {
+
+	Application::EnableVisualStyles();
+
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	PcClient::first form;
+
+	Application::Run(%form);
+
+}
