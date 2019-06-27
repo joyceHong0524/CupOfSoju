2d98d1f57c20d997771082b936d13a493990491f
joyceHong0524 <joyceHong0524@gmail.com>
2019-06-27 12:51:24 +0900

    Email-password firebase migrated

--- android/app/src/main/java/com/junga/cupofsoju/LogInActivity.kt
+++ android/app/src/main/java/com/junga/cupofsoju/LogInActivity.kt
@@ -2,11 +2,31 @@ package com.junga.cupofsoju
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_log_in.*
 import org.jetbrains.anko.startActivity
+import com.google.firebase.auth.FirebaseUser
+import androidx.annotation.NonNull
+import android.graphics.Color.DKGRAY
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.widget.Toast
+
 
 class LogInActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val TAG = "LogInActivity"
+
+
+    lateinit  var mAuth : FirebaseAuth;
+    lateinit var mAuthListener : FirebaseAuth.AuthStateListener;
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
@@ -14,6 +34,18 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
         button_google.setOnClickListener(this)
         button_login.setOnClickListener(this)
         textView_signup.setOnClickListener(this)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                Log.d(TAG, "onAuthStateChanged:signed_in:" + user.uid)
+            } else {
+                Log.d(TAG, "onAuthStateChanged:signed_out")
+            }
+            // updateUI(user)
+        }
 
     }
 
@@ -39,6 +71,23 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun emailLogin(){
+
+        val email = input_email.text.toString()
+        val password = input_password.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+            this
+        ) { task ->
+            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful)
+            if (!task.isSuccessful) {
+                Log.d(TAG,"tas k was not successful")
+            } else {
+                Log.d(TAG, "signInWithEmail:onComplete:" + "LOGED IN!!")
+                var user = mAuth.currentUser
+                Log.d(TAG,"current user Id : " + user!!.email)
+            }
+        }
+
 
     }
 }
