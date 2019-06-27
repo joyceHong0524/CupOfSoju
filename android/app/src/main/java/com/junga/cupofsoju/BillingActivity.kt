35190602fa634faf40a73ac55bf594ed66d2dcaf
joyceHong0524 <joyceHong0524@gmail.com>
2019-06-27 20:41:29 +0900

    Manifest bug fixed

---
+++ android/app/src/main/java/com/junga/cupofsoju/BillingActivity.kt
@@ -0,0 +1,14 @@
+package com.junga.cupofsoju
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class BillingActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_billing)
+
+    }
+}
