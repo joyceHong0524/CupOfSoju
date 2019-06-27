c44e1b1bf1dfaad8b441826ef6b7a48b3ee8e7a4
joyceHong0524 <joyceHong0524@gmail.com>
2019-06-27 13:39:01 +0900

    Google login completed

--- android/app/src/main/java/com/junga/cupofsoju/LogInActivity.kt
+++ android/app/src/main/java/com/junga/cupofsoju/LogInActivity.kt
@@ -14,7 +14,13 @@ import com.google.firebase.auth.AuthResult
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.OnCompleteListener
 import android.R.attr.password
+import android.content.Intent
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class LogInActivity : AppCompatActivity(), View.OnClickListener {
@@ -24,6 +30,7 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit  var mAuth : FirebaseAuth;
     lateinit var mAuthListener : FirebaseAuth.AuthStateListener;
+    lateinit var googleSignInClient: GoogleSignInClient;
 
 
 
@@ -67,8 +74,31 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
 
 
     private fun googleLogin(){
+        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id)) //Since R.stinrg.default_web_client_id keep not being resolved, I'm just using string value.
+            .requestEmail()
+            .build();
 
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        val signInIntent =googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,100)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==100){
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                //구글 로그인이 성공했을 경우
+                val account =result.signInAccount
+                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+            }
+        }
+    }
+
 
     private fun emailLogin(){
 
@@ -83,7 +113,7 @@ class LogInActivity : AppCompatActivity(), View.OnClickListener {
                 Log.d(TAG,"tas k was not successful")
             } else {
                 Log.d(TAG, "signInWithEmail:onComplete:" + "LOGED IN!!")
-                var user = mAuth.currentUser
+                val user = mAuth.currentUser
                 Log.d(TAG,"current user Id : " + user!!.email)
             }
         }
