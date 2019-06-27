4ef11bbae49ef61bfd4190ec3eb479958a1cb7e5
jungjune <willtriti03@naver.com>
2019-06-27 14:28:38 +0900

    Add owner java, signup xml

---
+++ android/app/src/main/java/com/junga/cupofsoju/Owner/OwnerMainView.java
@@ -0,0 +1,12 @@
+package com.junga.cupofsoju.Owner;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OwnerMainView extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ android/app/src/main/java/com/junga/cupofsoju/Owner/PermissionDenied.java
@@ -0,0 +1,16 @@
+package com.junga.cupofsoju.Owner;
+
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.junga.cupofsoju.R;
+
+public class PermissionDenied extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denied);
+
+    }
+}
