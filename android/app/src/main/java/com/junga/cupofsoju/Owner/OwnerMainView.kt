ca5506aee05d0f6da420471e3ba66f1a8662269d
joyceHong0524 <joyceHong0524@gmail.com>
2019-06-28 12:46:22 +0900

    license implemented

--- android/app/src/main/java/com/junga/cupofsoju/Owner/OwnerMainView.kt
+++ android/app/src/main/java/com/junga/cupofsoju/Owner/OwnerMainView.kt
@@ -1,5 +1,18 @@
 package com.junga.cupofsoju.Owner
 
+//Copyright 2019 Hanjanha Jo
+//
+//
+//Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+//file except in compliance with the License. You may obtain a copy of the License at
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software distributed
+//under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//express or implied. See the License for the specific language governing permissions and limitations under the License.
+//
+
+
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
@@ -29,6 +42,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_owner_main.*
 import kotlinx.android.synthetic.main.activity_owner_main.drawer
 import org.jetbrains.anko.startActivity
+
+
 
 
 class OwnerMainView : AppCompatActivity(), View.OnClickListener {
--- android/app/src/main/java/com/junga/cupofsoju/SearchListActivity.java
+++ android/app/src/main/java/com/junga/cupofsoju/SearchListActivity.java
@@ -1,5 +1,19 @@
 package com.junga.cupofsoju;
 
+
+//Copyright 2019 Hanjanha Jo
+//
+//
+//Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+//file except in compliance with the License. You may obtain a copy of the License at
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software distributed
+//under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//express or implied. See the License for the specific language governing permissions and limitations under the License.
+//
+
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -47,33 +61,33 @@ public class SearchListActivity extends AppCompatActivity {
         etSearchText = findViewById(R.id.search_edit);
         tvResultText = findViewById(R.id.search_text);
 
-        ivSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flResult.setLayoutParams(new TableLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 0, 1f));
-                tvResultText.setText("\"" + etSearchText.getText().toString() + "\" 검색결과");
-                myAdapter.clear();
-                storeInfo.clear();
-                db.collection("Store")
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        StoreData sd = document.toObject(StoreData.class);
-                                        if (sd.getLocation().contains(etSearchText.getText().toString())) {
-                                            storeInfo.add(sd);
-                                            myAdapter.notifyDataSetChanged();
-                                        }
-                                        myAdapter.resetting(storeInfo);
-                                    }
-                                }
-                            }
-                        });
-
-            }
-        });
+//        ivSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                flResult.setLayoutParams(new TableLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 0, 1f));
+//                tvResultText.setText("\"" + etSearchText.getText().toString() + "\" 검색결과");
+//                myAdapter.clear();
+//                storeInfo.clear();
+//                db.collection("Store")
+//                        .get()
+//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                                        StoreData sd = document.toObject(StoreData.class);
+//                                        if (sd.getLocation().contains(etSearchText.getText().toString())) {
+//                                            storeInfo.add(sd);
+//                                            myAdapter.notifyDataSetChanged();
+//                                        }
+//                                        myAdapter.resetting(storeInfo);
+//                                    }
+//                                }
+//                            }
+//                        });
+//
+//            }
+//        });
 
 
         db.collection("Store")
